235d5800046136f883a322be7f607497096d689e
fardosa-ibrahim <fardosaibrahimshide@gmail.com>
2022-04-08 19:21:40 +0300

    first commit

---
+++ Statements.py
@@ -0,0 +1,45 @@
+#   Boolean
+y=9
+print(y!=10)
+print(y<12)
+print(y>20)
+print(y==9)
+print(y<=7)
+print(y>=15)
+# if statements
+x=20
+if x>6:
+    print("It's greater than 6")
+num=16
+if num>7:
+    print("It's Bigger than 7")
+if num<=30:
+    print("It's between 7 and 30")
+    # elseIfStatements
+z=5
+if z==13:
+    print("Correct")
+else:
+    print("Incorrect")
+    # forMultiple checks
+    nums=3
+    if nums==1:
+        print("One")
+    else:
+        if nums==3:
+            print("Three")
+        else:
+         print("Something else")
+
+# elifStatement
+num=3
+if num==1:
+    print("One")
+elif num==2:
+    print("Two")
+elif num==3:
+    print("Three")
+else:
+    print("Something else")
+
+        
